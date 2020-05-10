5dd1abcf579678bc1e33d5f344a460b5c14389a2
mircoporetti <mircoporetti@gmail.com>
2020-05-10 16:50:43 +0200

    A guard for non negative integers

--- lib/gcd.exs
+++ lib/gcd.exs
@@ -1,6 +1,8 @@
 defmodule Gcd do
 
   def gcd(x,0), do: x
-  def gcd(x,y), do: gcd(y, rem(x,y))
+  def gcd(x,y) when is_integer(x) and x >= 0 and is_integer(y) and y >= 0 do
+    gcd(y, rem(x,y))
+  end
 
 end
