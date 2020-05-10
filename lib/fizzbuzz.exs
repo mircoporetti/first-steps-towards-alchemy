408c5f13f0e94ef42bd86ad8f5e876c39cbba1b1
mircoporetti <mircoporetti@gmail.com>
2020-05-10 16:02:01 +0200

    FizzBuzz module structure

--- lib/fizzbuzz.exs
+++ lib/fizzbuzz.exs
@@ -1,19 +1,14 @@
-fizz_buzz_calculator = fn
-  {0,0,_} -> "FizzBuzz"
-  {0, _, _} -> "Fizz"
-  {_, 0, _} -> "Buzz"
-  {_,_,c} -> c
+defmodule FizzBuzz do
+
+  def fizzBuzz(n) do
+    fizz_buzz_calculator = fn
+      {0,0,_} -> "FizzBuzz"
+      {0, _, _} -> "Fizz"
+      {_, 0, _} -> "Buzz"
+      {_,_,c} -> c
+    end
+
+    fizz_buzz_calculator.({rem(n,3), rem(n,5), n})
+  end
+
 end
-
-
-fizz_buzz = fn
-  n  -> fizz_buzz_calculator.({rem(n,3), rem(n,5), n})
-end
-
-IO.puts fizz_buzz.(10)
-IO.puts fizz_buzz.(11)
-IO.puts fizz_buzz.(12)
-IO.puts fizz_buzz.(13)
-IO.puts fizz_buzz.(14)
-IO.puts fizz_buzz.(15)
-IO.puts fizz_buzz.(16)
